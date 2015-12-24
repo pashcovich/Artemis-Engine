8c4596990ba51a1c26034760af3423ec847210e8
Michael Ala <Michael Ala>
2015-12-24 12:04:14 -0500

    Copied over Math library from previous engines (not finished).

---
+++ Artemis.Engine/Math/NewtonsMethodException.cs
@@ -0,0 +1,22 @@
+﻿#region Using Statements
+
+using System;
+using System.Runtime.Serialization;
+
+#endregion
+
+namespace Artemis.Engine.Math
+{
+
+    /// <summary>
+    /// An exception which occurs when NewtonsMethod cannot approximate or find the root of a given function.
+    /// </summary>
+    [Serializable]
+    public class NewtonsMethodException : Exception
+    {
+        public NewtonsMethodException() : base() { }
+        public NewtonsMethodException(string msg) : base(msg) { }
+        public NewtonsMethodException(string msg, Exception inner) : base(msg, inner) { }
+        public NewtonsMethodException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
