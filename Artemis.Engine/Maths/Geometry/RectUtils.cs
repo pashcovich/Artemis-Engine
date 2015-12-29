dd142c570061d124c5bf8f79bb17a1dea940b3eb
Michael Ala <michaelfonzolo@gmail.com>
2015-12-29 09:34:48 -0500

    Renamed math utilities to prevent name conflict with System.Maths.

---
+++ Artemis.Engine/Maths/Geometry/RectUtils.cs
@@ -0,0 +1,48 @@
+﻿
+namespace Artemis.Engine.Maths.Geometry
+{
+    public static class RectUtils
+    {
+
+        /// <summary>
+        /// Check if two rectangles are overlapping.
+        /// </summary>
+        public static bool Collision(
+            double x1, double y1, double w1, double h1,
+            double x2, double y2, double w2, double h2)
+        {
+            return x1 <= x2 + w2 &&
+                   x1 + w1 >= x2 &&
+                   y1 <= y2 + h2 &&
+                   y1 + h1 >= y2;
+        }
+
+        /// <summary>
+        /// Check if the first rectangle entirely encloses the second rectangle.
+        /// </summary>
+        public static bool Contains(
+            double x1, double y1, double w1, double h1,
+            double x2, double y2, double w2, double h2)
+        {
+            return x1 <= x2 && x1 + w1 >= x2 + w2 &&
+                   y1 <= y2 && y1 + h1 >= y2 + h2;
+        }
+
+        /// <summary>
+        /// Check if the two given rectangles form a cross pattern.
+        /// </summary>
+        public static bool Crosses(
+            double x1, double y1, double w1, double h1,
+            double x2, double y2, double w2, double h2)
+        {
+            return (
+                    x1 <= x2 && x1 + w1 >= x2 + w2 &&
+                    y1 >= y2 && y1 + h1 <= y2 + h2
+                ) || (
+                    x1 >= x2 && x1 + w1 <= x2 + w2 &&
+                    y1 <= y2 && y1 + h1 >= y2 + h2
+                );
+        }
+
+    }
+}
