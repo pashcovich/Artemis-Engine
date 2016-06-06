147c7c163ccd453710fda4ddfd1f8bf2eb845486
Michael Ala <michaelfonzolo@gmail.com>
2016-06-06 10:08:28 -0400

    Added class for `Body` extension methods.

---
+++ Artemis.Engine/BodyExtensions.cs
@@ -0,0 +1,25 @@
+﻿#region Using Statements
+
+using FarseerPhysics;
+using FarseerPhysics.Dynamics;
+
+using System.Linq;
+
+#endregion
+
+namespace Artemis.Engine
+{
+    public static class BodyExtensions
+    {
+        /// <summary>
+        /// Check if this body collides with the mouse.
+        /// </summary>
+        /// <param name="this"></param>
+        /// <returns></returns>
+        public static bool CollidingWithMouse(this Body @this)
+        {
+            var pos = ConvertUnits.ToSimUnits(ArtemisEngine.Mouse.PositionVector);
+            return @this.FixtureList.Any(f => f.TestPoint(pos));
+        }
+    }
+}
